b264604a43b6c0609a4d22d7b704d80363d2815c
bunturioto <Bunturioto@gmail.com>
2024-02-17 12:48:57 +0400

    define the decorator and wrapper inside the decorator

--- HW18.1.py
+++ HW18.1.py
@@ -1,3 +1,10 @@
+# Define the decorator function
+def decorator(func):
+    # Define the wrapper function inside the decorator
+    def wrapper(num):
+
+
+
 # Apply the decorator to the function is_positive
 @decorator
 def is_positive(num):
