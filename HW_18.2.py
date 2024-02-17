a0face5d0d55c2a348cf3fb834ab6f894d282d52
bunturioto <Bunturioto@gmail.com>
2024-02-17 13:22:50 +0400

    Check if the provided number is not positive and Call the instance of the Functor class

--- HW_18.2.py
+++ HW_18.2.py
@@ -4,8 +4,19 @@ class Functor:
         self.func = func
 
     def __call__(self, number):
+        # Check if the provided number is not positive
+        if number <= 0:
+            # Raise a ValueError if the number is not positive
+            raise ValueError("The number must be positive")
+        else:
+            return number
 
 
-# Define a function named is_positive, which simply returns the provided number
+# Define a function named is_positive
 def is_positive(num):
     return num
+
+
+my_func = Functor(is_positive)
+result = my_func(17)
+print(result)
