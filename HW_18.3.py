6c6df77808b19663b9bf182e548b22caa1aacc97
bunturioto <Bunturioto@gmail.com>
2024-02-17 13:30:07 +0400

    Set a commission fee,Check if the deposit after deducting the commission fee is sufficient for the transaction amount if not rise ValueError

--- HW_18.3.py
+++ HW_18.3.py
@@ -1,3 +1,14 @@
+# Define a decorator function
+def decorator(func):
+    def wrapper(deposit, amount):
+        # Set a commission fee
+        commission = 1
+        # Check if the deposit after deducting the commission fee
+        # is sufficient for the transaction amount if not rise ValueError
+        if deposit - commission >= amount:
+            return "Payment completed successfully"
+        else:
+            raise ValueError("There is insufficient amount in the account")
 
 
 # Apply the decorator to the transaction function
