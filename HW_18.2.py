94ce8c7988e08021fc2a46f68f249751ca42cac3
bunturioto <Bunturioto@gmail.com>
2024-02-17 13:19:20 +0400

    Define a class named Functor Define the init and call method

--- HW_18.2.py
+++ HW_18.2.py
@@ -1,3 +1,11 @@
+# Define a class named Functor
+class Functor:
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, number):
+
+
 # Define a function named is_positive, which simply returns the provided number
 def is_positive(num):
     return num
