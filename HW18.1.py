e1d295d8ad908349d107c2f82afb8fee101350c2
bunturioto <Bunturioto@gmail.com>
2024-02-17 12:52:44 +0400

    Call the decorated function

--- HW18.1.py
+++ HW18.1.py
@@ -13,9 +13,11 @@ def decorator(func):
     return wrapper
 
 
-
 # Apply the decorator to the function is_positive
 @decorator
 def is_positive(num):
-    # Simply return the number
     return num
+
+
+# Call the decorated function
+is_positive(5)
